8ba846e966816f5c3b632b23da0034088bcfc0da
Ian Baumeister <ian@avotravel.com>
2024-05-18 11:23:36 +0200

    Added 5 Pokemons with their key features

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
 // scripts.js
 
-alert('Hello world');
-
-var favoriteFood = 'Chicken';
-
-// Use document.write() to display the value stored in favoriteFood
-document.write('My favorite food is: ' + favoriteFood);
-
+let pokemonList = [
+    {
+        name: "Azumarill",
+        height: 0.8,
+        types: ['fairy', 'water']
+    },
+    {
+        name: "Charmander",
+        height: 0.6,
+        types: ['fire']
+    },
+    {
+        name: "Charizard",
+        height: 1.7,
+        types: ['fire', 'flying']
+    },
+    {
+        name: "Sandslash",
+        height: 1,
+        types: ['ground']
+    },
+    {
+        name: "Charmeleon",
+        height: 1.1,
+        types: ['fire']
+    }
+];
